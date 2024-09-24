5d2fbc185d74f761b2565bb527829e839fdf0577
Meta Fusion Labs <metafusionlabs@gmail.com>
2024-09-24 20:32:09 +0100

    update 001

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure the base is correctly set for production
 })
